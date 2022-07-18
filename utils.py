0a32174320f051903ab8d2773279fc82f6f1bbf8
Yurii Trihub <yuriitrihub@gmail.com>
2022-07-18 14:51:49 +0300

    добавлены функции поиска по имени в utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import json
 url = "http://mypictures.me/123"
 
 
-def load_candidates():
+def load_candidates_from_json():
     """которая загрузит данные из файла"""
     with open('candidates.json', mode='r', encoding='utf-8') as f:
         candidates = json.load(f)
@@ -17,24 +17,38 @@ def get_all(candidates):
         return candidates_list
 
 
-def get_by_pk(pk):
+def get_candidate(candidate_id):
     """которая вернет кандидата по pk"""
-    candidates = load_candidates()
+    candidates = load_candidates_from_json()
     for j in candidates:
-        if pk == j['pk']:
-            candidate_by_pk = f"""<img src='({url})'> \n
+        if id == j['id']:
+            candidate_by_id = f"""<img src='({url})'> \n
                                 <pre> \n
                                     {j['name']}\n
                                     {j['position']}\n
                                     {j['skills']}\n
                                     \n
                                 </pre>"""
-            return candidate_by_pk
+            return candidate_by_id
+
+def get_candidates_by_name(candidate_name):
+    """которая возвращает кандидатов по имени"""
+    candidates = load_candidates_from_json()
+    for g in candidates:
+        if candidate_name == g['name']:
+            candidate_by_name = f"""<img src='({url})'> \n
+                                <pre> \n
+                                    {j['name']}\n
+                                    {j['position']}\n
+                                    {j['skills']}\n
+                                    \n
+                                </pre>"""
+            return candidate_by_name
 
 
-def get_by_skill(skill_name):
+def get_candidates_by_skill(skill_name):
     """которая вернет кандидатов по навыку"""
-    candidates = load_candidates()
+    candidates = load_candidates_from_json()
     for k in candidates:
         if skill_name in k['skills']:
             candidate_by_skill = f"""<img src='({url})'> \n
